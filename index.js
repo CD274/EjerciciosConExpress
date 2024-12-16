9cc6573c828fe492c56e831227daad1ef9205d4d
Carlos <gordillod274@gmail.com>
2024-12-16 10:49:44 -0500

    Se aumentó los mtodos de respuesta tipo GET

--- index.js
+++ index.js
@@ -18,6 +18,23 @@ app.get('/nombre',(req,resp)=>{
     resp.json(lista);
 });
 
+app.get('/Carlos',(req,resp)=>{
+    resp.send("Mi nombre es Carlos Gordillo, tengo 20 años y estoy estudiando Desarrollo de Software en el IST 17 de Julio");
+});
+app.get('/suma',(req,resp)=>{
+    let a = 10; 
+    let b = 5;
+    let n = a+b; 
+    resp.send("Total:"+n);
+});
+app.get('/suma/:n1',(req,resp)=>{
+    let a = parseInt(req.params.n1); 
+    let b = 5;
+    let n = a+b; 
+    resp.send("Total:"+n);
+});
+
 app.listen(3000,()=>{
     console.log('Servidor corriendo en el puerto 3000');  //Notificación del inicio del server
 });
+
